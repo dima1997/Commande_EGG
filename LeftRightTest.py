58298dd8f11e64c6d55fe9c6725b0d13f8c827d4
dima1997 <franki.dimaria@gmail.com>
2022-09-19 23:33:01 +0200

    Refactor: colors in Lef

--- LeftRightTest.py
+++ LeftRightTest.py
@@ -1,25 +1,16 @@
 import pygame
 from Tools.FontRender import RenderFont
+import colors as C
 
 
 class LeftRightTest(object):
     def __init__(self, screen):
-        #############
-        # Colours
-        #############
-        self.LBLUE = (0, 204, 204)
-        self.BLACK = (0, 0, 0)
-        self.RED = (180, 30, 30)
-        self.GREEN = (30, 180, 30)
-        self.GREY = (150, 150, 150)
-        self.LGREY = (200, 200, 200)
-        self.WHITE = (252, 252, 252)
         self.screen = screen
         self.on_race_menu = True
         self.screen_width, self.screen_height = pygame.display.get_surface().get_size()
         self.button_list = []
-        self.templeft = RenderFont("L", 300, self.WHITE)
-        self.tempright = RenderFont("R", 300, self.WHITE)
+        self.templeft = RenderFont("L", 300, C.WHITE)
+        self.tempright = RenderFont("R", 300, C.WHITE)
 
     def DrawButtons(self):
 
@@ -39,25 +30,25 @@ class LeftRightTest(object):
             alpha_detected = False
             beta_detected = True
             mx, my = pygame.mouse.get_pos()
-            self.screen.fill(self.LGREY)
-            pygame.draw.rect(self.screen, self.RED, self.button_list[2])
-            pygame.draw.circle(self.screen, self.GREY, self.button_list[0].center, 200)
-            pygame.draw.circle(self.screen, self.GREY, self.button_list[1].center, 200)
+            self.screen.fill(C.LGREY)
+            pygame.draw.rect(self.screen, C.RED, self.button_list[2])
+            pygame.draw.circle(self.screen, C.GREY, self.button_list[0].center, 200)
+            pygame.draw.circle(self.screen, C.GREY, self.button_list[1].center, 200)
             click = False
             self.screen.blit(self.templeft, [self.button_list[0].center[0]-75,self.button_list[0].center[1]-150])
             self.screen.blit(self.tempright, [self.button_list[1].center[0]-80,self.button_list[2].center[1]-150])
 
-            pygame.draw.rect(self.screen, self.GREEN, self.button_list[3], 15)
+            pygame.draw.rect(self.screen, C.GREEN, self.button_list[3], 15)
 
             for event in pygame.event.get():
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         self.on_race_menu = False
                     if event.key == pygame.K_RIGHT:
-                        print("right")
+                        print("Right")
                         self.button_list[3].move_ip(20,0)
                     if event.key == pygame.K_LEFT:
-                        print("left")
+                        print("Left")
                         self.button_list[3].move_ip(-20, 0)
 
             pygame.display.update()
