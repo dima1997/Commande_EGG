3ea718a9745258838f3d595d8ae216c649542a85
dima1997 <franki.dimaria@gmail.com>
2022-11-10 20:31:52 +0100

    refactor: config.py for constants

--- My_project/main.py
+++ My_project/main.py
@@ -1,22 +1,10 @@
 import pygame, sys
 from pygame.locals import *
-import random
+from config import *
  
 pygame.init()
  
-FPS = 60
 FramePerSec = pygame.time.Clock()
- 
-# Predefined some colors
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
- 
-# Screen information
-SCREEN_WIDTH = 400
-SCREEN_HEIGHT = 600
 
 infoObject = pygame.display.Info()
 DISPLAYSURF = pygame.display.set_mode(
@@ -24,14 +12,6 @@ DISPLAYSURF = pygame.display.set_mode(
     pygame.RESIZABLE)
 DISPLAYSURF.fill(BLACK)
 pygame.display.set_caption("SSVEP Stimulation")
-
-BOX_SCREEN_FRACTION = 0.1
-BOX_CENTER_Y_SCREEN_FRACTION = 0.5
-RED_BOX_CENTER_X_SCREEN_FRACTION = 0.25
-BLUE_BOX_CENTER_X_SCREEN_FRACTION = 0.75
-
-RED_FREQUENCY = 15
-BLUE_FREQUENCY = 25
 
 class Box:
     def __init__(self, color, center_x_surface_fraction, frequency, current_time):
