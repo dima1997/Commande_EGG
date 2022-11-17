47da54ad2f64219d0059192ccb1233f540f53042
dima1997 <franki.dimaria@gmail.com>
2022-11-17 16:42:44 +0100

    add: multiple customisables boxes

--- SSVEP_Interface/main.py
+++ SSVEP_Interface/main.py
@@ -14,16 +14,18 @@ DISPLAYSURF.fill(BLACK)
 pygame.display.set_caption("SSVEP Stimulation")
 
 class Box:
-    def __init__(self, color, center_x_surface_fraction, frequency, current_time):
+    def __init__(self, color, x_fraction, y_fraction, side_fraction, frequency, current_time):
         self.color = color
-        self.center_x_surface_fraction = center_x_surface_fraction
+        self.x_fraction = x_fraction
+        self.y_fraction = y_fraction
+        self.side_fraction = side_fraction
         self.delay = ((1/frequency)*1000) / 2
-        self.time = current_time + self.delay
+        self.change_time = current_time + self.delay
         self.show = True
 
     def update(self, current_time):
-        if current_time >= self.time:
-             self.time = current_time + self.delay
+        if current_time >= self.change_time:
+             self.change_time = current_time + self.delay
              self.show = not self.show
 
     def draw(self, surface):
@@ -32,10 +34,10 @@ class Box:
 
         surface_width, surface_height = surface.get_size()
 
-        side_size = min(surface_width, surface_height) * BOX_SCREEN_FRACTION 
+        side_size = min(surface_width, surface_height) * self.side_fraction
 
-        x = (surface_width * self.center_x_surface_fraction) - (side_size / 2)
-        y = (surface_height * BOX_CENTER_Y_SCREEN_FRACTION) - (side_size / 2)
+        x = (surface_width * self.x_fraction) - (side_size / 2)
+        y = (surface_height * self.y_fraction) - (side_size / 2)
         w = side_size
         z = side_size
 
@@ -43,8 +45,12 @@ class Box:
 
 def main():
     current_time = pygame.time.get_ticks()
-    red_box = Box(RED, RED_BOX_CENTER_X_SCREEN_FRACTION, RED_FREQUENCY, current_time)
-    blue_box = Box(BLUE, BLUE_BOX_CENTER_X_SCREEN_FRACTION, BLUE_FREQUENCY, current_time)
+    boxes = []
+    for box_data in BOXES:
+        box = Box(box_data[0], box_data[1], 
+                    box_data[2], box_data[3], 
+                    box_data[4], current_time)
+        boxes.append(box)
 
     while True:     
         for event in pygame.event.get(): 
@@ -55,13 +61,13 @@ def main():
         current_time = pygame.time.get_ticks()
 
         # Update objects
-        red_box.update(current_time)
-        blue_box.update(current_time)
+        for box in boxes:
+            box.update(current_time)
         
         # Make objects update screen
         DISPLAYSURF.fill(BLACK)
-        red_box.draw(DISPLAYSURF)
-        blue_box.draw(DISPLAYSURF)
+        for box in boxes:
+            box.draw(DISPLAYSURF)
                 
         pygame.display.update()
         FramePerSec.tick(FPS)
